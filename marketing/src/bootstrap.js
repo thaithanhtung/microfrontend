4baad80ae6f5c2546cf4c7aa20a8a3fb5fb5f4b2
TungThai <tungtt@clik.vn>
2023-10-10 17:53:21 +0700

    push code

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Mount  function to start up th app
+const mount = (el) => {
+  console.log("run mount");
+
+  if (!el) return;
+  ReactDOM.render(<App />, el);
+};
+
+// if we are in development and in isolation
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_markerting-dev-root");
+
+  console.log("run run", devRoot);
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+export { mount };
