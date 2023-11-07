354a8108edd434282ca47acd90d4d1e47ea3c92f
TungThai <tungtt@clik.vn>
2023-11-07 23:49:55 +0700

    add env add code 2

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -8,6 +8,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publisPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
