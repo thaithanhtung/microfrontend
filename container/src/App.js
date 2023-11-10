a76bedd2804dcb35a6b473c01d8d2880160898fe
TungThai <tungtt@clik.vn>
2023-11-10 16:09:37 +0700

    add header css

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
+import { create } from "domain";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 export default () => {
   console.log("render app nha");
   return (
